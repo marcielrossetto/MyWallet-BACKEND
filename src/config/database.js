e035316cc6968fab5415894c6bd7319679b0e253
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2025-05-15 19:07:28 -0300

    Config Banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { MongoClient } from 'mongodb';
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+const client = new MongoClient(process.env.MONGO_URI);
+await client.connect();
+
+const db = client.db();
+
+export const getDB = () => db;
