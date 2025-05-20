8e85d8917d6932339ae8e6a1759fe116a789962c
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2025-05-20 01:12:45 -0300

    troca url front  para testar render4

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ import transactionRouter from './routers/transactionRouter.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://marcielrossetto.github.io',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(express.json());
 
-app.use(authRouter); // Sem prefixo
+app.use(authRouter);
 app.use(transactionRouter);
 
 export default app;
