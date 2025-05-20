f2cfac370bdd81b9567834ff2899040c6e16de25
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2025-05-20 00:42:38 -0300

    troca url front  para testar render

--- src/server.js
+++ src/server.js
@@ -1,4 +1,15 @@
-import app from './app.js';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://marcielrossetto.github.io',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
+// outras configurações e rotas aqui...
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
