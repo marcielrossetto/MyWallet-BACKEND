d4adabafd96ae736335325b0a3e51c8e4972101c
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2025-05-20 01:26:21 -0300

    troca url front  para testar render5

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRouter from './routers/authRouter.js';
-import transactionRouter from './routers/transactionRouter.js';
 
 dotenv.config();
 
@@ -14,6 +13,5 @@ app.use(cors({
 app.use(express.json());
 
 app.use(authRouter);
-app.use(transactionRouter);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import app from './app.js';
 
 const app = express();
 
